458f846e379f7f880c9a8ffd489d27532ac1aa23
Matheus <mathgiuliano@gmail.com>
2020-08-23 13:20:56 -0300

    Término da aula 06 - 23.08

---
+++ Aprendendo CSharp/Fatorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando fatorial");
+
+            int fatorial = 1;
+
+            for (int n = 1; n <= 10; n++)
+            {
+                fatorial *= n;
+                Console.WriteLine("Fatorial de " + n + " = " + fatorial);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aprendendo CSharp/ImparPar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImparPar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Impar ou Par");
+
+            //Instanciação da variável inicializadora
+            int numero = 0;
+
+            //Enquanto o número for menor que 100
+            while (numero <= 100)
+            {
+                //Se o mod do número for igual a zero, é par
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("O número " + numero + " é par");
+                    numero++;
+                }
+                //Se o mod for diferente de zero, é ímpar
+                else
+                {
+                    Console.WriteLine("O número " + numero + " é impar");
+                    numero++;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
