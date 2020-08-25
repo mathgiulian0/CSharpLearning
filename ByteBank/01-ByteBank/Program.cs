90156b47d40ad2e0a41e93d37a5baebfc99b80db
Matheus <mathgiuliano@gmail.com>
2020-08-24 21:32:00 -0300

    C# parte 2 - Término da aula 01-02 - 24.08.2020

---
+++ ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente = new ContaCorrente();
+
+            contaCorrente.titular = "Gabriela";
+            contaCorrente.agencia = 863;
+            contaCorrente.numero = 863146;
+            contaCorrente.saldo = 100;
+
+
+            Console.WriteLine("Titular: " + contaCorrente.titular);
+            Console.WriteLine("Agência: " + contaCorrente.agencia);
+            Console.WriteLine("Número: " + contaCorrente.numero);
+            Console.WriteLine("Saldo: " + contaCorrente.saldo);
+
+            contaCorrente.saldo += 200;
+
+            Console.WriteLine("Saldo: " + contaCorrente.saldo);
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaCorrente = new ContaCorrente();
+            contaCorrente.titular = "Gabriela";
+            contaCorrente.agencia = 863;
+            contaCorrente.numero = 863452;
+
+
+            ContaCorrente contaCorrente2 = new ContaCorrente();
+            contaCorrente.titular = "Gabriela";
+            contaCorrente.agencia = 863;
+            contaCorrente.numero = 863452;
+
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaCorrente == contaCorrente2));
+
+            contaCorrente = contaCorrente2;
+            Console.WriteLine(contaCorrente == contaCorrente2);
+
+            contaCorrente.saldo = 300;
+            Console.WriteLine(contaCorrente.saldo);
+            Console.WriteLine(contaCorrente2.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
+
