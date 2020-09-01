70b7b3247c09ac9e7eee81d2be4f399b11c60f58
Matheus <mathgiuliano@gmail.com>
2020-08-31 22:03:15 -0300

    ProjetoUdemy - 31.08.2020

---
+++ Aprendendo CSharp/UdemyProjeto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string a = vet[0];
+            string b = vet[1];
+            string c = vet[2];
+
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+        }
+    }
+}
