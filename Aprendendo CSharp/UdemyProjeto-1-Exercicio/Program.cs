b88b982871065593b50fca21faaac577f146e8ac
Matheus <mathgiuliano@gmail.com>
2020-09-02 22:28:49 -0300

    ProjetoUdemy - 02.09.2020

---
+++ Aprendendo CSharp/UdemyProjeto-1-Exercicio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace UdemyProjeto_1_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o seu nome completo: ");
+            string nomeCompleto = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa? ");
+            int quartosCasa = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto: ");
+            double precoProduto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com o seu último nome, idade e altura (mesma linha): ");
+            string[] vet = Console.ReadLine().Split(' ');
+            string ultimoNome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(quartosCasa);
+            Console.WriteLine(precoProduto);
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
