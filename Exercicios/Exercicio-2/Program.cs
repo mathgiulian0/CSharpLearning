07892befa6c04332ac6d79a1a8fbbba90c003560
Matheus <mathgiuliano@gmail.com>
2020-09-04 17:45:19 -0300

    Exercicios - 04.09.2020

---
+++ Exercicios/Exercicio-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            Console.WriteLine("Qual é o seu nome?");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Olá, " + nome + ", é um prazer te conhecer!");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicios/Exercicio-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeFuncionario;
+            double salario;
+
+            Console.WriteLine("Nome do Funcionário: ");
+            nomeFuncionario = Console.ReadLine();
+
+            Console.WriteLine("Salário: ");
+            salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("O funcionário " + nomeFuncionario + " tem um salário de " + salario + " em Junho.");
+
+            Console.ReadLine();
+        }
+    }
+}
