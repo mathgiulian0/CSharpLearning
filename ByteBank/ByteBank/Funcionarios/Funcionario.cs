6b8f0cd8acc0bab96e99cdd3ece57afd4d842069
Matheus <matgiuliano@gmail.com>
2020-09-22 22:42:39 -0300

    Commit 22.09.2020

---
+++ ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    class Funcionario
+    {
+        // 0 - Funcionario
+        // 1 - Diretor
+        // 2 - Designer
+        // 3 - Gerente de conta corrente 
+        // 4 - Coordenador  
+        // N - ...
+        private int _tipo;
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+        public double GetBonificacao()
+        {
+            if (_tipo == 1)
+                return Salario;
+          
+            return Salario * 0.10;
+        }
+    }
+}
