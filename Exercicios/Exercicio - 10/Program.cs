91db0adc0a88a83d8615ec4b0350235eca4f198a
Matheus <matgiuliano@gmail.com>
2021-01-30 04:41:47 -0300

    JogoDaVelha

---
+++ Exercicios/Exercicio - 10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio___10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, altura, m2, tinta;
+
+            Console.WriteLine("Largura: ");
+            largura = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Altura: ");
+            altura = Double.Parse(Console.ReadLine());
+
+            m2 = altura * largura;
+
+            Console.WriteLine("Sua parede tem a dimensão de " + largura + "x" + altura + " e a sua área é de " + m2 + "m²");
+
+            tinta = m2 / 2;
+
+            Console.WriteLine("Você poderá pintar " + tinta + " metros quadrados!");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicios/Exercicio-09/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double real, dolar;
+
+            Console.WriteLine("Digite a quantidade que você possui na carteira: ");
+            real = Double.Parse(Console.ReadLine());
+
+            dolar = real / 3.45;
+
+            Console.WriteLine("Você pode comprar " + "$" + dolar + " dólares!");
+
+            Console.ReadLine();
+        }
+    }
+}
